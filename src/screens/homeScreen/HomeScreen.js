ef2e98139a8548f1604755aed8f3898bf11ab7ff
Martinsky Michael <michael@lmksytem.fr>
2022-11-15 10:56:47 +0100

    correction image et indentation

--- src/screens/homeScreen/HomeScreen.js
+++ src/screens/homeScreen/HomeScreen.js
@@ -2,13 +2,12 @@ import React from 'react'
 import json from '../../data/logements.json';
 import ItemLogement from '../../components/itemLogement/ItemLogement'
 import './HomeScreen.css';
-import bandeauImg from '../../assets/images/bandeau.svg';
 
 const HomeScreen = () => {
 
     return (
         <div>
-            <div style={{backgroundImage: `url(${bandeauImg})`}} className='img-container'>
+            <div style={{}} className='img-container'>
                 <h1>Chez vous, partout et ailleurs</h1>
             </div>
             <div className='list'>
